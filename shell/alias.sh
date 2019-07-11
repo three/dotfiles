d8a83016b9031ca4d6dd0f51f8cab973fc06fe4b
Eric Roberts <eric@threedot14.com>
2019-07-11 17:36:39 -0400

    Fix bad aliases

--- shell/alias.sh
+++ shell/alias.sh
@@ -9,3 +9,7 @@ alias gc="git commit -v"
 alias gd="git diff"
 alias ga="git add --all"
 alias gs="git status"
+
+# Clipboard
+alias xc="xclip -selection clipboard -i"
+alias xp="xclip -selection clipboard -o"
--- shell/utilities.sh
+++ shell/utilities.sh
@@ -1,4 +1,6 @@
 edit_fzf_git() {(
     cd "$(git rev-parse --show-toplevel || return $?)"
-    "$EDITOR" "$(git ls-files -oc --exclude-standard | fzf --preview 'bat --color always {1}')"
+    FILE_TO_EDIT="$(git ls-files -oc --exclude-standard | fzf --preview 'bat --color always {1}')"
+    [[ ! $? = 0 ]] && return $?
+    "$EDITOR" "$FILE_TO_EDIT"
 )}
